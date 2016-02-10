f3eb92cc5ca8e89ae18cf806be9dbc7d8e6ebd7c
nuc134r <chelman.soft@gmail.com>
2016-02-10 23:50:11 +0300

    Добавление модели данных

---
+++ KioskClient/Model/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+namespace KioskClient.Model
+{
+    public class Movie
+    {
+        public Movie(int id, string title, string plot, int duration, BitmapImage poster, List<int> showtimes,
+            List<string> genres, DateTime releaseDate)
+        {
+            Id = id;
+            Title = title;
+            Plot = plot;
+            Duration = duration;
+            Poster = poster;
+            Showtimes = showtimes;
+            Genres = genres;
+            ReleaseDate = releaseDate;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Plot { get; private set; }
+        public int Duration { get; private set; }
+        public BitmapImage Poster { get; private set; }
+        public List<int> Showtimes { get; private set; }
+        public List<string> Genres { get; private set; }
+        public DateTime ReleaseDate { get; private set; }
+    }
+}
---
+++ KioskClient/Model/MovieBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+namespace KioskClient.Model
+{
+    public class MovieBuilder
+    {
+        private int duration;
+
+        private List<string> genres;
+        private int id;
+        private string plot;
+        private BitmapImage poster;
+        private DateTime releaseDate;
+        private List<int> showtimes;
+        private string title;
+
+        public MovieBuilder WithId(int id)
+        {
+            this.id = id;
+            return this;
+        }
+
+        public MovieBuilder WithTitle(string title)
+        {
+            this.title = title;
+            return this;
+        }
+
+        public MovieBuilder WithPlot(string plot)
+        {
+            this.plot = plot;
+            return this;
+        }
+
+        public MovieBuilder WithDuration(int duration)
+        {
+            this.duration = duration;
+            return this;
+        }
+
+        public MovieBuilder WithPoster(BitmapImage poster)
+        {
+            this.poster = poster;
+            return this;
+        }
+
+        public MovieBuilder WithShowtimes(List<int> showtimes)
+        {
+            this.showtimes = showtimes;
+            return this;
+        }
+
+        public MovieBuilder WithGenres(List<string> genres)
+        {
+            this.genres = genres;
+            return this;
+        }
+
+        public MovieBuilder WithReleaseDate(DateTime releaseDate)
+        {
+            this.releaseDate = releaseDate;
+            return this;
+        }
+
+        public Movie Please()
+        {
+            return new Movie(id, title, plot, duration, poster, showtimes, genres, releaseDate);
+        }
+    }
+}
