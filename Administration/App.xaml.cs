3f35a1c117c290849423a0bb49405827bcc1fba7
nuc134r <chelman.soft@gmail.com>
2016-03-01 22:00:07 +0300

    Мелочи по интерфейсу

--- Administration/App.xaml.cs
+++ Administration/App.xaml.cs
@@ -1,15 +1,31 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Markup;
 
 namespace Administration
 {
-    /// <summary>
-    ///     Interaction logic for App.xaml
-    /// </summary>
     public partial class App
     {
+        private static void ConfigureCulture()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.CurrentCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.CurrentCulture;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(System.Windows.Documents.Run),
+                new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            ConfigureCulture();
 
             var mainWindow = new MainWindow();
             var loginWindow = new LoginWindow();
