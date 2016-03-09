c63567c8274ec5af4a6d53d5f997ca74647a6261
nuc134r <chelman.soft@gmail.com>
2016-03-09 22:53:39 +0300

    Вёрстка и отладка логики списка сеансов

--- DataAccess/Model/Showtime.cs
+++ DataAccess/Model/Showtime.cs
@@ -10,5 +10,30 @@ namespace DataAccess.Model
         public Auditorium Auditorium { get; set; }
         public int Price { get; set; }
         public bool ThreeD { get; set; }
+
+        public string AdditionalTimeString
+        {
+            get
+            {
+                var minutesLeft = (Time - DateTime.Now).TotalMinutes;
+                if (minutesLeft <= 15 && minutesLeft > 0)
+                {
+                    return string.Format(Resources.TimeLeftString, minutesLeft);
+                }
+
+                var isTomorrow = DateTime.Today + TimeSpan.FromDays(1) == Time.Date;
+                if (isTomorrow)
+                {
+                    return "завтра";
+                }
+
+                return "";
+            }
+        }
+
+        public string ThreeDeeLabelText
+        {
+            get { return ThreeD ? "3D" : ""; }
+        }
     }
 }
