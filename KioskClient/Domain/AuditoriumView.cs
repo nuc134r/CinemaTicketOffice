bdd5ba80012a84034cc30f0b8eb991a6b28590b5
nuc134r <chelman.soft@gmail.com>
2016-04-01 02:25:40 +0300

    Задел для механизма выбора сеанса

--- KioskClient/Domain/AuditoriumView.cs
+++ KioskClient/Domain/AuditoriumView.cs
@@ -1,14 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DataAccess.Annotations;
 using DataAccess.Model;
 
 namespace KioskClient.Domain
 {
-    public class AuditoriumSeat
+    public class AuditoriumSeat : INotifyPropertyChanged
     {
+        private bool isSelected;
         public int SeatNumber { get; set; }
         public AuditoriumRow Row { get; set; }
-        public bool IsSelected { get; set; }
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
         public bool IsFree { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
     public class AuditoriumRow
@@ -17,12 +41,8 @@ namespace KioskClient.Domain
         public List<AuditoriumSeat> Seats { get; set; }
     }
 
-    public class AuditoriumView
+    public class AuditoriumView : INotifyPropertyChanged
     {
-        public AuditoriumView()
-        {
-        }
-
         public AuditoriumView(Auditorium auditorium)
         {
             Rows = new List<AuditoriumRow>();
@@ -37,13 +57,28 @@ namespace KioskClient.Domain
 
                 for (var j = 0; j < auditorium.Seats; j++)
                 {
-                    row.Seats.Add(new AuditoriumSeat {Row = row, SeatNumber = j + 1, IsFree = true});
+                    var seat = new AuditoriumSeat
+                    {
+                        Row = row,
+                        SeatNumber = j + 1,
+                        IsFree = true
+                    };
+                    seat.PropertyChanged += SeatOnPropertyChanged;
+
+                    row.Seats.Add(seat);
                 }
 
                 Rows.Add(row);
             }
         }
 
+        private void SeatOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(sender, new PropertyChangedEventArgs(propertyChangedEventArgs.PropertyName));
+        }
+
         public List<AuditoriumRow> Rows { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- KioskClient/View/AuditoriumMapPage.xaml.cs
+++ KioskClient/View/AuditoriumMapPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using DataAccess.Model;
 using KioskClient.Domain;
@@ -7,16 +9,31 @@ namespace KioskClient.View
 {
     public partial class AuditoriumMapPage
     {
+        private readonly AuditoriumMapPageViewModel viewModel;
+
         public AuditoriumMapPage(Showtime showtime)
         {
             InitializeComponent();
 
-            DataContext = new AuditoriumView(showtime.Auditorium);
+            var auditoriumView = new AuditoriumView(showtime.Auditorium);
+            auditoriumView.PropertyChanged += AuditoriumViewOnPropertyChanged;
+            DataContext = auditoriumView;
+        }
+
+        private void AuditoriumViewOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            var seat = (AuditoriumSeat)sender;
+            MessageBox.Show(seat.Row.Number + " " + seat.SeatNumber);
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             ((MainWindow)Window.GetWindow(this)).NavigateBack();
         }
+    }
+
+    public class AuditoriumMapPageViewModel
+    {
+
     }
 }
