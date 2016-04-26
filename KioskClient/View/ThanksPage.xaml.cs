8d80f3108dc28980083288267805c0a56428ef9e
nuc134r <chelman.soft@gmail.com>
2016-04-26 22:58:27 +0300

    Перенос логики во ViewModel

--- KioskClient/View/ThanksPage.xaml.cs
+++ KioskClient/View/ThanksPage.xaml.cs
@@ -1,44 +1,22 @@
-﻿using System.Timers;
-using System.Windows;
+﻿using System.Windows;
 using KioskClient.ViewModel;
 
 namespace KioskClient.View
 {
     public partial class ThanksPage
     {
-        private readonly Timer delayTimer;
         private readonly ThanksPageViewModel viewModel;
-        private Timer timer;
-
+        
         public ThanksPage()
         {
             InitializeComponent();
 
             viewModel = new ThanksPageViewModel(this);
-
-            delayTimer = new Timer(1500);
-            delayTimer.Elapsed += DelayTimerOnElapsed;
         }
 
         private void ThanksPage_OnLoaded(object sender, RoutedEventArgs e)
         {
-            delayTimer.Start();
-        }
-
-        private void DelayTimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
-        {
-            delayTimer.Stop();
-
-            //SystemDriveConnection.EjectDiskE();
-
-            timer = new Timer(2000);
-            timer.Elapsed += (o, eventArgs) =>
-            {
-                timer.Stop();
-                Dispatcher.Invoke(viewModel.GoToCatalogPage);
-            };
-
-            timer.Start();
+            viewModel.StartTimers();
         }
     }
 }
--- KioskClient/ViewModel/ThanksPageViewModel.cs
+++ KioskClient/ViewModel/ThanksPageViewModel.cs
@@ -1,17 +1,40 @@
-﻿using System.Windows.Controls;
+﻿using System.Timers;
+using System.Windows.Controls;
 
 namespace KioskClient.ViewModel
 {
     public class ThanksPageViewModel : ViewModelBase
     {
+        private readonly Timer delayTimer;
+        private Timer timer;
+
         public ThanksPageViewModel(Page view)
         {
             this.view = view;
+
+            delayTimer = new Timer(1500);
+            delayTimer.Elapsed += DelayTimerOnElapsed;
         }
 
-        public void GoToCatalogPage()
+        public void StartTimers()
         {
-            Window.NavigateToMovieCatalog();
+            delayTimer.Start();
+        }
+
+        private void DelayTimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            delayTimer.Stop();
+
+            //SystemDriveConnection.EjectDiskE();
+
+            timer = new Timer(2000);
+            timer.Elapsed += (o, eventArgs) =>
+            {
+                timer.Stop();
+                view.Dispatcher.Invoke(() => { Window.NavigateToMovieCatalog(); });
+            };
+
+            timer.Start();
         }
     }
 }
